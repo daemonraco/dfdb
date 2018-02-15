0e57c39316b1c8066b1bd3678bb6639ba21a0e22
DAEMonRaco <daemonraco@gmail.com>
2018-02-15 20:39:25 -0300

    Code documentation (#6)
    * Adding some code documentation to:
    ** 'interface.resource.dfdb.ts'
    * Bug fixes.

--- lib/includes/interface.resource.dfdb.d.ts
+++ lib/includes/interface.resource.dfdb.d.ts
@@ -2,12 +2,59 @@
  * @file interface.resource.dfdb.ts
  * @author Alejandro D. Simi
  */
+import { Promise } from 'es6-promise';
+/**
+ * This interfase specifies the required methods of an asset that represents a
+ * file.
+ *
+ * @interface IResource
+ */
 export interface IResource {
-    connect(done: any): void;
-    close(done: any): void;
+    /**
+     * Creating a resource representation object doesn't mean it is connected to
+     * physical information, this method must do that.
+     * It connects and loads information from the physical storage in zip file.
+     *
+     * @method connect
+     * @returns {Promise<void>} Return a promise that gets resolved when the
+     * operation finishes.
+     */
+    connect(): Promise<void>;
+    /**
+     * Provides a way to save current status and then close this resource.
+     *
+     * @method close
+     * @returns {Promise<void>} Return a promise that gets resolved when the
+     * operation finishes.
+     */
+    close(): Promise<void>;
+    /**
+     * Provides a way to know if there was an error in the last operation.
+     *
+     * @method error
+     * @returns {boolean} Returns TRUE when there was an error.
+     */
     error(): boolean;
-    lastError(): string;
+    /**
+     * Provides access to the error message registed by the last operation.
+     *
+     * @method lastError
+     * @returns {string|null} Returns an error message.
+     */
+    lastError(): string | null;
 }
+/**
+ * This interfase specifies the required methods of an asset that represents a
+ * file and may not save information the first time is asked.
+ *
+ * @interface IResource
+ */
 export interface IDelayedResource {
+    /**
+     * When the physical file saving is trigger by a later action, this method
+     * avoids next file save attempt for this resource.
+     *
+     * @method skipSave
+     */
     skipSave(): void;
 }
